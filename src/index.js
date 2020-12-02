49df69d3bd19940a662298a79dc4bdc65e579595
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-02 00:03:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let input=document.getElementById('new-task-description');
 let form=document.getElementById('create-task-form');
 let tasklist=[];
 form.addEventListener("submit", function(e){
-  e.preventDefault
+  e.preventDefault(e);
   let submittedTask="";
   submittedTask=input.value;
   taskList.push(submittedTask);
