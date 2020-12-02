ae65eff920df10bf3a7b4d94bb93ad0c8efc1dca
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-02 00:32:08 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let tasks=document.getElementById('tasks');
+let input=document.getElementById('new-task-description');
+let form=document.getElementById('create-task-form');
+let tasklist=[];
+form.addEventListener("submit", function(e){
+  let submittedTask="";
+  submittedTask=input.value;
+  taskList.push(submittedTask);
+  addList(taskList);
+  e.preventDefault(e);
 });
+
+function addList(a){let list="";
+ for(let i=0; i<a.length; i++){
+   list+=`<li>`+a[i]+`</li>`;}
+   tasks.innerHTML=list;}
